754f49a6791aa331b132d50f5a10c94c31ddf85c
Miguel Lopes <miguel.lopes@miniclip.com>
2020-04-04 15:54:22 +0100

    expose avilable extensions and create instance

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
+#include <vector>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -29,13 +30,20 @@ public:
 private:
     void initWindow() {
         glfwInit();
+        int ret = glfwInit();
+        printf("glfwInit ret: %d\n", ret);
+        if (!glfwVulkanSupported()) {
+             printf("Vulkan support not found\n");
+        }else{
+            printf("Vulkan is supported, yay!\n");
+        }
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
     
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -45,12 +53,64 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
-
         glfwTerminate();
+    }
+    
+    void createInstance() {
+        
+#pragma region available_extensions
+        ///Helper functions to demonstrate the available extensions
+        //total extension count
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        std::cout << extensionCount << "Number of extensions supported: "<< extensionCount << std::endl;
+        std::cout << "available extensions:" << std::endl;
+        for (const auto& extension : extensions) {
+            std::cout << "\t" << extension.extensionName << std::endl;
+        }
+#pragma endregion
+        //load global extensions
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+                
+        /* Info vkCreateInstance:
+         - Pointer to struct with creation info
+         - Pointer to custom allocator callbacks, always nullptr in this tutorial
+         - Pointer to the variable that stores the handle to the new object
+         */
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (result == VK_ERROR_INCOMPATIBLE_DRIVER) {
+            std::cout << "cannot find a compatible Vulkan ICD\n";
+            throw std::runtime_error("failed to create instance!");
+        } else  if (result != VK_SUCCESS) {
+            std::cout << "Uknown error\n";
+            throw std::runtime_error("failed to create instance!");
+        }
     }
 private:
     GLFWwindow* window;
+    VkInstance instance;
 };
 
 int main() {
@@ -65,4 +125,3 @@ int main() {
 
     return EXIT_SUCCESS;
 }
-
