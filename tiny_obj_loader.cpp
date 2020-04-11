7103a40293a446c568e644def0c7306fe6411f56
Miguel Lopes <miguel.lopes@miniclip.com>
2020-04-11 11:51:34 +0100

    Load model

---
+++ tiny_obj_loader.cpp
@@ -0,0 +1,10 @@
+//
+//  tiny_obj_loader.cpp
+//  VulkanMacos
+//
+//  Created by Miguel Lopes on 10/04/2020.
+//  Copyright © 2020 Miguel Lopes. All rights reserved.
+//
+
+#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+#include "tiny_obj_loader.h"
